4ff20785d22913d6466c6337e8da10849b559268
Dessy-tom <83626152+Dessy-tom@users.noreply.github.com>
2021-06-07 19:00:56 +0900

    修正しました

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,12 +3,12 @@ FactoryBot.define do
     association :user
     name                { 'test' }
     text                { 'test' }
-    category_id         { '2' }
-    condition_id        { '2' }
-    shipping_charge_id  { '2' }
-    shipping_area_id    { '2' }
-    shipping_day_id     { '2' }
-    price               { '1000' }
+    category_id         { 2 }
+    condition_id        { 2 }
+    shipping_charge_id  { 2 }
+    shipping_area_id    { 2 }
+    shipping_day_id     { 2 }
+    price               { 1000 }
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -22,37 +22,37 @@ RSpec.describe Item, type: :model do
     end
     
     it 'カテゴリーが空では登録できない' do
-      @item.category_id = '1'
+      @item.category_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Category must be other than 1")
     end
 
     it '商品の状態が空では登録できない' do
-      @item.condition_id = '1'
+      @item.condition_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Condition must be other than 1")
     end
 
     it '配送料の負担が空では登録できない' do
-      @item.shipping_charge_id = '1'
+      @item.shipping_charge_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping charge must be other than 1")
     end
 
     it '発送元の地域が空では登録できない' do
-      @item.shipping_area_id = '1'
+      @item.shipping_area_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping area must be other than 1")
     end
 
     it '発送までの日数が空では登録できない' do
-      @item.shipping_day_id = '1'
+      @item.shipping_day_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping day must be other than 1")
     end
 
     it '価格が空では登録できな' do
-      @item.price = ''
+      @item.price = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Price can't be blank")
     end
@@ -70,13 +70,13 @@ RSpec.describe Item, type: :model do
     end
 
     it '価格が300円以下だと登録できない' do
-      @item.price = '200'
+      @item.price = 200
       @item.valid?
       expect(@item.errors.full_messages).to include("Price must be greater than 300")
     end
 
     it '価格が9999999以上だと登録できない' do
-      @item.price = '10000000'
+      @item.price = 10000000
       @item.valid?
       expect(@item.errors.full_messages).to include("Price must be less than 9999999")
     end
